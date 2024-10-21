803905facc5d17adb9b687d68205352edf58a8a5
Mezbaur Are Rafi <mezbaur2004@gmail.com>
2024-10-21 23:42:27 +0600

    login link added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="container">
                 <p className="text-center text-light">
-                    &copy; 2024 All rights reserved by Rafi
+                    &copy; 2024 All rights reserved by Rafi<Link to='/admin'><span className="admin-link">.</span></Link>
                 </p>
             </div>
         </footer>
