c037c5ee320868aa3221250b90da5787788560b0
Mezbaur Are Rafi <mezbaur2004@gmail.com>
2024-10-27 01:19:41 +0600

    re

--- src/assets/variables.js
+++ src/assets/variables.js
@@ -1,3 +1,3 @@
-const URL='https://backend-portfolio-gvya5xszx-mezbaur-are-rafis-projects.vercel.app/api'|| import.meta.env.VITE_API_BASE_URL ;
+const URL='https://backend-af6pdptvf-mezbaur-are-rafis-projects.vercel.app/api'|| import.meta.env.VITE_API_BASE_URL ;
 
 export default URL;
