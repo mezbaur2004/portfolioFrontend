bd08f6dc4bb4a0bdde5222d75deb4672b138a7de
Mezbaur Are Rafi <mezbaur2004@gmail.com>
2026-07-04 10:04:52 +0600

    Update footer to display current year dynamically

--- src/components/main/footer.jsx
+++ src/components/main/footer.jsx
@@ -6,8 +6,10 @@ const Footer = () => {
         <footer className="footer">
             <div className="container">
                 <p className="text-center text-light">
-                    &copy; 2024 All rights reserved by Rafi<Link to='/admin'><span className="admin-link">.</span></Link>
-                </p>
+    &copy; {new Date().getFullYear()} All rights reserved by Rafi
+    <Link to='/admin'><span className="admin-link">.</span></Link>
+</p>
+
             </div>
         </footer>
     );
