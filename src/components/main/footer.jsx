52a6ae9184768999a17ac99c532b5ead5b6fcfd8
Mezbaur Are Rafi <mezbaur2004@gmail.com>
2026-08-01 20:31:41 +0600

    Refactor footer component structure and styling

--- src/components/main/footer.jsx
+++ src/components/main/footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import '../../css/footer.css';
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="container">
-                <p className="text-center text-light">
-    &copy; {new Date().getFullYear()} All rights reserved by Rafi
-    <Link to='/admin'><span className="admin-link">.</span></Link>
-</p>
-
+            <div className="section-container">
+                <p className="footer-text">
+                    &copy; {new Date().getFullYear()} All rights reserved by Rafi
+                    <Link to='/admin'><span className="admin-link">.</span></Link>
+                </p>
             </div>
         </footer>
     );
